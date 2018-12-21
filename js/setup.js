63372602fff5658ad20dcf45a10d2eb86367c676
VVMaestro <38831071+VVMaestro@users.noreply.github.com>
2018-12-21 17:20:05 +0600

    Добавленная логика наполнения массива  персонажами случайными данными

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,10 @@
+var CHARACTERS_NUMBER = 4;
 var setup = document.querySelector(".setup");
 var firstNames = ["Иван", "Хуан Себастьян", "Мария", "Кристоф", "Виктор", "Юлия", "Люпита", "Вашингтон"];
 var secondNames = ["да Марья", "Верон", "Мирабелла", "Вальц", "Онопко", "Топольницкая", "Нионго", "Ирвинг"];
+var coatColors = ["rgb(101, 137, 164)", "rgb(241, 43, 107)", "rgb(146, 100, 161)", "rgb(56, 159, 117)", "rgb(215, 210, 55)", "rgb(0, 0, 0)"];
+var eyesColors = ["black", "red", "blue", "yellow", "green"];
+var characters = new Array();
 
 var getRandomNumber = function(min, max) {
     var randomValue = Math.floor(Math.random() * (max - min) + min);
@@ -20,20 +24,20 @@ var getRandomName = function(arrOfFirstN, arrOfSecondN) {
     } else return randomSecondName + "\u00A0" + randomFirstName;
 }
 
-
-var characters = [
-    {
-        name: "name"
-    },
-    {
-        name: "name"
-    },
-    {
-        name: "name"
-    },
-    {
-        name: "name"
+class Character {
+    constructor(name, coatColor, eyesColor) {
+        this.name = name;
+        this.coatColor = coatColor;
+        this.eyesColor = eyesColor;
     }
-];
+}
+
+for (i = 0; i < CHARACTERS_NUMBER; i++) {
+    characters[i] = new Character(
+        getRandomName(firstNames, secondNames),
+        coatColors[getRandomNumber(0, coatColors.length)],
+        eyesColors[getRandomNumber(0, eyesColors.length)]
+    );
+}
 
 setup.classList.remove("hidden");
