18f1d715d841f35fb1d79992786b9768622741df
Valentine <bmaecmpo@gmail.com>
2018-12-21 09:41:41 +0500

    Добавляет исправления

--- js/game-mode.js
+++ js/game-mode.js
@@ -1,1 +1,21 @@
 var fireballSize = 22;
+var getFireballSpeed = function(left) {
+    if (left === true) {
+        return 5;
+    } else return 2;
+}
+
+var wizardSpeed = 3;
+var wizardWidth = 70;
+
+var getWizardHeight = function() {
+    return 1.337 * wizardWidth;
+}
+
+var getWizardX = function(width) {
+    return (width / 2) - (wizardWidth / 2);
+}
+
+var getWizardY = function(height) {
+    return height / 3; 
+}
